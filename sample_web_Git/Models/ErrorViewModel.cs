6ed7f943739c4e27ad47a276052ba40bf7bb2ac3
Chowdary <manne.chowdary@sap.com>
2023-04-16 07:50:52 +0530

    initial push
    initial push

---
+++ sample_web_Git/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sample_web_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
